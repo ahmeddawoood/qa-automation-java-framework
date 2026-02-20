376ada547c041e5a05c285e214f54c007aa865bf
ahmeddawoood <163445492+ahmeddawoood@users.noreply.github.com>
2026-02-21 01:09:34 +0200

    Add FirstTest class with a basic test case

---
+++ src/test/java/tests/ui/FirstTest.java
@@ -0,0 +1,12 @@
+package tests.ui;
+
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
+
+public class FirstTest {
+
+    @Test
+    void first_test_runs() {
+        assertEquals(2, 1 + 1);
+    }
+}
